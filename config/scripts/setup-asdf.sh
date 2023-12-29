af4330617ba35dca38e54db5e2dbeceaeed5af11
Karl 'NaN' Wikström <10465598+HerrNaN@users.noreply.github.com>
2023-12-29 10:30:46 +0100

    fix(just): use syntax for `just` 1.20 (#22)

--- config/scripts/setup-asdf.sh
+++ config/scripts/setup-asdf.sh
@@ -19,7 +19,9 @@ echo '2. Downloading source'
 git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch v0.13.1
 
 echo '3. Install (for fish)'
+mkdir -p $FISH_DIR
 echo "source $ASDF_DIR/asdf.fish" >> $FISH_DIR/config.fish
 
 echo '4. Add completions (for fish)'
-mkdir -p $FISH_DIR/completions && ln -s $ASDF_DIR/completions/asdf.fish $FISH_DIR/completions
+mkdir -p $FISH_DIR/completions
+ln -s $ASDF_DIR/completions/asdf.fish $FISH_DIR/completions
